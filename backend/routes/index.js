f90d7e4b012fc375eff5a2d471ae1c57fa8841fa
Sarah <sarahashih@gmail.com>
2024-11-20 12:24:47 -0800

    backend setup phase 1.1

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
 
 router.get("/api/csrf/restore", (req, res) => {
     const csrfToken = req.csrfToken();
@@ -10,3 +13,6 @@ router.get("/api/csrf/restore", (req, res) => {
   });
 
 module.exports = router;
+
+
+

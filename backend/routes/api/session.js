34abe2df61054450f7b2fb0829d3c5850524d99f
Sarah <sarahashih@gmail.com>
2024-11-20 14:01:24 -0800

    backend setup phase 5

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -7,8 +7,21 @@ const bcrypt = require('bcryptjs');
 
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 
-// Log in
+
+const validateLogin = [
+    check('credential')
+      .exists({ checkFalsy: true })
+      .notEmpty()
+      .withMessage('Please provide a valid email or username.'),
+    check('password')
+      .exists({ checkFalsy: true })
+      .withMessage('Please provide a password.'),
+    handleValidationErrors
+  ];
+
 
 router.get(
     '/',
@@ -26,9 +39,11 @@ router.get(
       } else return res.json({ user: null });
     }
   );
-  
+
+// Log in
 router.post(
     '/',
+    validateLogin,
     async (req, res, next) => {
       const { credential, password } = req.body;
   
@@ -61,7 +76,7 @@ router.post(
         user: safeUser
       });
     }
-);
+  );
 
 // Log out
 router.delete(

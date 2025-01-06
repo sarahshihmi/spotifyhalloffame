2a9ae9bc01323987611fab31b5c6205d81150f60
Sarah <sarahashih@gmail.com>
2025-01-05 22:13:20 -0800

    all the deploys are deploying deploying

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,20 +1,38 @@
 const config = require('./index');
 
+console.log(process.env.USE_LOCAL_POSTGRESS === 'true');
 module.exports = {
   development: {
-    storage: 'db/dev.db',
-    dialect: "sqlite",
+    storage: config.dbFile,
+
+    ...(process.env.USE_LOCAL_POSTGRESS === 'true' ? {
+      use_env_variable: 'DATABASE_URL',
+        dialectOptions: {
+      },
+      define: {
+          schema: process.env.SCHEMA
+      },
+    } : {
+      dialect: "sqlite",
+    }),
+    
     seederStorage: "sequelize",
     logQueryParameters: true,
     typeValidation: true
   },
-  production: {
+  production: process.env.USE_PROD_SQLITE ? {
+    storage: config.dbFile,
+    dialect: "sqlite",
+    seederStorage: "sequelize",
+    logQueryParameters: true,
+    typeValidation: true
+  } : {
     use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
     seederStorage: 'sequelize',
     dialectOptions: {
       ssl: {
-        require: true,
+        require: false,
         rejectUnauthorized: false
       }
     },
